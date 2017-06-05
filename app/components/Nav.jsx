b36657eb187ac08647b15f39755f06c54c5943f1
eli fridman <thisiselimail@gmail.com>
2017-06-05 22:11:26 +0300

    init app

---
+++ app/components/Nav.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+var {Link,IndexLink} = require('react-router');
+
+var Nav = ()=>{
+  return(
+    <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Time App</li>
+            <li><IndexLink to="/" activeClassName="active">Timer</IndexLink></li>
+            <li><Link to="/" activeClassName="active">CountDown</Link></li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+         <ul className="menu">
+           <li className="menu-text">Created By<Link to="/">Eli Fridman</Link></li>
+         </ul>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Nav;
